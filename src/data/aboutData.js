2ac25642c309eb48475399e813e7c46988df2e88
harshpandloskar <38388253+harshpandloskar@users.noreply.github.com>
2023-02-15 14:40:01 +0000

    Textual change

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Bhavesh. I'm a front-end developer based in UK.",
-    description2: "Versatile Front-End Developer with 2+ years of experience implementing complex web apps and internal frameworks, specializing in SPA development utilizing React ecosystem. Eager to tackle more complex problems and continue to find ways to maximize user efficiency. I spend my leisure hours playing all kind of sports and partying. I love to learn and explore new arenas.",
+    description2: "Versatile Front-End Developer with 4+ years of experience implementing complex web apps and internal frameworks, specializing in SPA development utilizing React ecosystem. Eager to tackle more complex problems and continue to find ways to maximize user efficiency. I spend my leisure hours playing all kind of sports and partying. I love to learn and explore new arenas.",
     image: 2
 }
