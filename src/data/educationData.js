e25e332f95c037417d1179c2d83d4971d6557ede
harshpandloskar <38388253+harshpandloskar@users.noreply.github.com>
2023-02-15 14:28:12 +0000

    update build for gh-pages

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -3,28 +3,28 @@ export const educationData = [
         id: 1,
         institution: 'Smt Sulochanadevi Singhania School',
         course: 'ICSE',
-        startYear: '1999',
-        endYear: '2011'
+        startYear: 'June 1999',
+        endYear: 'May 2011'
     },
     {
         id: 2,
         institution: 'Smt Sulochanadevi Singhania School',
         course: 'ISC',
-        startYear: '2011',
-        endYear: '2013'
+        startYear: 'June 2011',
+        endYear: 'May 2013'
     },
     {
         id: 3,
         institution: 'Saraswati College of Engineering',
         course: 'Bachelor of Engineering in Computer Science',
-        startYear: '2013',
-        endYear: '2015'
+        startYear: 'July 2013',
+        endYear: 'May 2015'
     },
     {
         id: 4,
         institution: 'Loughborough University',
         course: 'Master of Science in Artificial Intelligence',
-        startYear: '2021',
-        endYear: 'Present'
+        startYear: 'Oct 2021',
+        endYear: 'Sept 2022'
     },
 ]
